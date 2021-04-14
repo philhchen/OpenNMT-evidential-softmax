Rename evidential softmax function and fix eval issue

onmt/modules/global_attention.py
@@ -8,7 +8,7 @@ from onmt.modules.sparse_activations import (
     sparsemax_topk,
     tsallis15,
     tsallis15_topk,
-    sparsesoftmax,
+    esoftmax,
 )
 
 from onmt.modules.root_finding import tsallis_bisect, sparsemax_bisect
@@ -99,7 +99,7 @@ class GlobalAttention(nn.Module):
             "sparsemax",
             "tsallis15",
             "tsallis",
-            "sparsesoftmax",
+            "esoftmax",
         ), "Please select a valid attention function."
         self.attn_func = attn_func
 
@@ -175,8 +175,8 @@ class GlobalAttention(nn.Module):
         if self.attn_func == "softmax":
             return F.softmax(Z, -1)
 
-        elif self.attn_func == "sparsesoftmax":
-            return sparsesoftmax(Z, -1)
+        elif self.attn_func == "esoftmax":
+            return esoftmax(Z, -1)
 
         elif self.attn_func == "sparsemax":
             return sparsemax(Z, -1)

onmt/modules/multi_headed_attn.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 from onmt.modules.sparse_activations import (
-    SparseSoftmax,
+    ESoftmax,
     Sparsemax,
     Tsallis15,
     TsallisBisect,
@@ -77,8 +77,8 @@ class MultiHeadedAttention(nn.Module):
 
         if attn_func == "softmax":
             self.normalization = nn.Softmax(dim=-1)
-        elif attn_func == "sparsesoftmax":
-            self.normalization = SparseSoftmax(dim=-1)
+        elif attn_func == "esoftmax":
+            self.normalization = ESoftmax(dim=-1)
         elif attn_func == "sparsemax":
             self.normalization = Sparsemax(dim=-1)
         elif attn_func == "tsallis15":

onmt/modules/sparse_activations.py
@@ -370,50 +370,36 @@ class LogSparsemaxBisect(nn.Module):
 INF = 1e6
 
 
-def log_sparsesoftmax(
-    input: torch.Tensor, dim: int, training: bool = True
-) -> torch.Tensor:
+def log_esoftmax(input: torch.Tensor, dim: int, training: bool = True) -> torch.Tensor:
     mask = input < torch.mean(input, dim=dim, keepdim=True)
-    mask_offset = mask * (INF if training else float("Inf"))
+    mask_offset = torch.zeros(input.shape, device=input.device, dtype=input.dtype)
+    mask_offset[mask] = INF if training else float("Inf")
     log_probs = F.log_softmax(input - mask_offset, dim=dim)
     return log_probs
 
 
-def sparsesoftmax(input: torch.Tensor, dim: int, training: bool = True) -> torch.Tensor:
+def esoftmax(input: torch.Tensor, dim: int, training: bool = True) -> torch.Tensor:
     mask = input < torch.mean(input, dim=dim, keepdim=True)
-    mask_offset = mask * (INF if training else float("Inf"))
+    # mask_offset = mask * (INF if training else float("Inf"))
+    mask_offset = torch.zeros(input.shape, device=input.device, dtype=input.dtype)
+    mask_offset[mask] = INF if training else float("Inf")
     probs = F.softmax(input - mask_offset, dim=dim)
     return probs
 
 
-class LogSparseSoftmax(torch.nn.Module):
-    def __init__(self, dim: int = -1, training: bool = True):
-        super(LogSparseSoftmax, self).__init__()
+class LogESoftmax(torch.nn.Module):
+    def __init__(self, dim: int = -1):
+        super(LogESoftmax, self).__init__()
         self.dim = dim
-        self.training = training
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        return log_sparsesoftmax(X, self.dim, self.training)
+        return log_esoftmax(X, self.dim, self.training)
 
 
-class SparseSoftmax(torch.nn.Module):
-    def __init__(self, dim: int = -1, training: bool = True):
-        super(SparseSoftmax, self).__init__()
+class ESoftmax(torch.nn.Module):
+    def __init__(self, dim: int = -1):
+        super(ESoftmax, self).__init__()
         self.dim = dim
-        self.training = training
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        return sparsesoftmax(X, self.dim, self.training)
-
-
-class SparseSoftmaxLoss(torch.nn.Module):
-    def __init__(self, reduction: str = "none", dim: int = -1):
-        super(SparseSoftmaxLoss, self).__init__()
-        self.log_sparse_softmax = LogSparseSoftmax(dim)
-        self.reduction = reduction
-        self.dim = dim
-
-    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        return F.nll_loss(
-            self.log_sparse_softmax(input), target, reduction=self.reduction
-        )
+        return esoftmax(X, self.dim, self.training)

onmt/utils/loss.py
@@ -13,6 +13,7 @@ import onmt.inputters as inputters
 from onmt.utils.logging import logger
 
 from onmt.modules.sparse_losses import (
+    ESoftmaxLoss,
     SparsemaxLoss,
     Tsallis15Loss,
     SparsemaxTopKLoss,
@@ -20,8 +21,6 @@ from onmt.modules.sparse_losses import (
     SparsemaxBisectLoss,
     TsallisBisectLoss,
 )
-
-from onmt.modules.sparse_activations import LogSparsemax
 
 
 def build_loss_compute(model, tgt_vocab, opt, train=True):
@@ -41,36 +40,39 @@ def build_loss_compute(model, tgt_vocab, opt, train=True):
     assert (
         opt.k == 0 or opt.bisect_iter == 0
     ), "Bisection and topk are mutually exclusive !"
-    if opt.loss_alpha == 1:
-        criterion = nn.CrossEntropyLoss(ignore_index=padding_idx, reduction="sum")
-    elif opt.loss_alpha == 2.0:
-        # sparsemax
-        if opt.k > 0:
-            criterion = SparsemaxTopKLoss(
-                k=opt.k, ignore_index=padding_idx, reduction="sum"
-            )
-        elif opt.bisect_iter > 0:
-            criterion = SparsemaxBisectLoss(
-                n_iter=opt.bisect_iter, ignore_index=padding_idx, reduction="sum"
-            )
-        else:
-            criterion = SparsemaxLoss(ignore_index=padding_idx, reduction="sum")
-    elif opt.loss_alpha == 1.5 and opt.bisect_iter == 0:
-        # tsallis 1.5, non-bisection cases
-        if opt.k > 0:
-            criterion = Tsallis15TopKLoss(
-                k=opt.k, ignore_index=padding_idx, reduction="sum"
-            )
-        else:
-            criterion = Tsallis15Loss(ignore_index=padding_idx, reduction="sum")
+    if opt.loss_fn == "esoftmax":
+        criterion = ESoftmaxLoss(ignore_index=padding_idx, reduction="sum")
     else:
-        # generic tsallis with bisection
-        criterion = TsallisBisectLoss(
-            alpha=opt.loss_alpha,
-            n_iter=opt.bisect_iter,
-            ignore_index=padding_idx,
-            reduction="sum",
-        )
+        if opt.loss_alpha == 1:
+            criterion = nn.CrossEntropyLoss(ignore_index=padding_idx, reduction="sum")
+        elif opt.loss_alpha == 2.0:
+            # sparsemax
+            if opt.k > 0:
+                criterion = SparsemaxTopKLoss(
+                    k=opt.k, ignore_index=padding_idx, reduction="sum"
+                )
+            elif opt.bisect_iter > 0:
+                criterion = SparsemaxBisectLoss(
+                    n_iter=opt.bisect_iter, ignore_index=padding_idx, reduction="sum"
+                )
+            else:
+                criterion = SparsemaxLoss(ignore_index=padding_idx, reduction="sum")
+        elif opt.loss_alpha == 1.5 and opt.bisect_iter == 0:
+            # tsallis 1.5, non-bisection cases
+            if opt.k > 0:
+                criterion = Tsallis15TopKLoss(
+                    k=opt.k, ignore_index=padding_idx, reduction="sum"
+                )
+            else:
+                criterion = Tsallis15Loss(ignore_index=padding_idx, reduction="sum")
+        else:
+            # generic tsallis with bisection
+            criterion = TsallisBisectLoss(
+                alpha=opt.loss_alpha,
+                n_iter=opt.bisect_iter,
+                ignore_index=padding_idx,
+                reduction="sum",
+            )
 
     criterion_name = str(type(criterion))
     # now all loss functions operate on raw logits